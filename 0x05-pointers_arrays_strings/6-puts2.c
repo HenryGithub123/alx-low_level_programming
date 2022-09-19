3482b652371cdd397ecab3fcaffaf897000cacf6
HenryGithub123 <ohenryedu123@gmail.com>
2022-09-19 10:04:09 -0700

    put2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,30 +1,18 @@
 #include "main.h"
-
 #include <string.h>
 
-
-
 /**
-
 * puts2 -> puts2 function
-
 * @str: str parameter
-
+* Return: 0 (success)
 */
 
 void puts2(char *str)
-
 {
-
 	int len, i;
 
-
-
 	len = strlen(str);
-
 	for (i = 0; i < len; i += 2)
-
 		_putchar(str[i]);
-
 	_putchar('\n');
 }
